e538bb54cda5a6854c0677be0774e2cad5bf1feb
waghmodesairaj45 <sairajwaghmode45@gmail.com>
2026-09-17 11:03:45 +0530

    Add files via upload

---
+++ IterableList.py
@@ -0,0 +1,6 @@
+Data=[11,21,51,101]
+
+
+print("Data From list is :")
+for no in Data:
+    print(no)
---
+++ IterableListX.py
@@ -0,0 +1,6 @@
+Data=[11,21,51,101]
+
+
+print("Data From list is :")
+for i in range(len(Data)):
+    print(Data[i])
