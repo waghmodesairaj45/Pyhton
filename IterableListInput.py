5cfccbdb8faca300e8ca32fd018f2ed33e198afd
waghmodesairaj45 <sairajwaghmode45@gmail.com>
2026-09-25 18:29:49 +0530

    Add files via upload

---
+++ IterableListInput.py
@@ -0,0 +1,12 @@
+def main():
+    marks=[]
+
+    marks.append(11)
+    marks.append(21)
+    marks.append(51)
+    marks.append(101)
+
+    print(marks)
+
+if __name__=="__main__":
+    main()
---
+++ IterableListInputXX.py
@@ -0,0 +1,24 @@
+def main():
+    marks=list()
+    
+    print("Enter 5 marks :")
+
+    no=int(input())
+    marks.append(no)
+
+    no=int(input())
+    marks.append(no)
+
+    no=int(input())
+    marks.append(no)
+
+    no=int(input())
+    marks.append(no)
+    
+    no=int(input())
+    marks.append(no)
+
+    print(marks)
+
+if __name__=="__main__":
+    main()
